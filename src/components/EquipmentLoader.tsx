ce063de795be36f16ffaa1738fe3a462267944e8
Xavier FOLCH <xavier.folch@esker.com>
2021-10-08 13:52:11 +0200

    add EquipmentLoader component

---
+++ src/components/EquipmentLoader.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react"
+
+export function EquipmentLoader() {
+    return (
+        <div>
+            <Button type="submit" id="load-button">Load Kigard data</Button>
+        </div>
+    )
+}
